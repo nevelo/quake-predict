d7c0842e71b4bccc40a3ea17214272bb65449c75
Mark <mlipski@mail.uoguelph.ca>
2017-04-13 17:59:34 -0400

    Further improved neuralnet

--- LSTMtest.py
+++ LSTMtest.py
@@ -22,7 +22,7 @@ import csv
 import random
 import tensorflow as tf
 import tensorflow.contrib.layers as layers
-from my_lstm_cell import MyLSTMCell as myCell
+from my_lstm_cell import EQLSTM as myCell
 import os
 import urllib2
 import matplotlib.pyplot as plt
@@ -63,17 +63,18 @@ def acquireData():
                 highMag[i] = row[7]
             i += 1
 
-        return t, pwr, meanPwr, highMag
+    highMag[highMag > 1] = 1
+    return t, pwr, meanPwr, highMag
 
 ################################################################################
 ##                           GRAPH DEFINITION                                 ##
 ################################################################################
 
-INPUT_SIZE    = 1       # 2 bits per timestep
-RNN_HIDDEN    = 100
+INPUT_SIZE    = 2       # 2 bits per timestep
+RNN_HIDDEN    = 200
 OUTPUT_SIZE   = 1       # 1 bit per timestep
 TINY          = 1e-6    # to avoid NaNs in logs
-LEARNING_RATE = 0.01
+LEARNING_RATE = 0.02
 
 acquireData()
 
@@ -85,6 +86,7 @@ outputs = tf.placeholder(tf.float32, (None, None, OUTPUT_SIZE)) # (time, batch, 
 cell = myCell(RNN_HIDDEN)
 
 batch_size    = tf.shape(inputs)[1]
+print(batch_size)
 initial_state = cell.zero_state(batch_size, tf.float32)
 
 rnn_outputs, rnn_states = tf.nn.dynamic_rnn(cell, inputs, initial_state=initial_state, time_major=True)
@@ -92,13 +94,12 @@ rnn_outputs, rnn_states = tf.nn.dynamic_rnn(cell, inputs, initial_state=initial_
 
 final_projection = lambda x: layers.linear(x, num_outputs=OUTPUT_SIZE, activation_fn=tf.nn.sigmoid)
 
-WOutput = tf.Variable(0, name="output_Weighting", dtype="float32")
 
 # apply projection to every timestep.
-predicted_outputs = map_fn(final_projection, rnn_outputs*WOutput)
+predicted_outputs = map_fn(final_projection, rnn_outputs)
 
 # compute elementwise cross entropy.
-error = -(outputs * tf.log(predicted_outputs + TINY) + (1.0 - outputs) * tf.log(1.0 - predicted_outputs + TINY))
+error = -(outputs*1 * tf.log(predicted_outputs + TINY) + (1.0 - outputs) * tf.log(1.0 - predicted_outputs + TINY))
 error = tf.reduce_mean(error)
 
 # optimize
@@ -117,39 +118,41 @@ res = predicted_outputs
 ################################################################################
 
 ITERATIONS_PER_EPOCH = 100
-BATCH_SIZE = 30
+base = 2
+BATCH_SIZE = int(100 * base)
+step = 50
+n = int(7500/step)
+m = int(18/base)
+n_ = int(75/base)
 
 t, pwr, meanPWR, highMag = acquireData()
 size = np.size(t)
 
-x = np.empty((240, BATCH_SIZE, 1))
-y = np.empty((240, BATCH_SIZE, 1))
+x = np.empty((n, BATCH_SIZE, INPUT_SIZE))
+y = np.empty((n, BATCH_SIZE, OUTPUT_SIZE))
 
-xTest = np.empty((60, BATCH_SIZE, 1))
-yTest = np.empty((60, BATCH_SIZE, 1))
+xTest = np.empty((m, BATCH_SIZE, INPUT_SIZE))
+yTest = np.empty((m, BATCH_SIZE, OUTPUT_SIZE))
 
-for j in range(240):
+for j in range(n):
+    for i in np.arange(BATCH_SIZE):
+        x[j,i, 0] = pwr[i + j*step]
+        x[j, i, 1] = meanPWR[i + j*step]
+        y[j, i, 0] = highMag[i + j * step]
+
+for j in range(n_, (n_+m)):
     for i in range(BATCH_SIZE):
-        x[j,i, 0] = pwr[i + j*BATCH_SIZE]
+        xTest[j-n_,i, 0] = pwr[i + j*BATCH_SIZE]
+        xTest[j-n_, i, 1] = meanPWR[i + j*BATCH_SIZE]
+        yTest[j - n_, i, 0] = highMag[i + j*BATCH_SIZE]
 
-for j in range(240, 300):
-    for i in range(BATCH_SIZE):
-        xTest[j-240,i, 0] = pwr[i + j*BATCH_SIZE]
-
-for j in range(240):
-    for i in range(BATCH_SIZE):
-        y[j,i, 0] = highMag[i + j*BATCH_SIZE]
-
-for j in range(240, 300):
-    for i in range(BATCH_SIZE):
-        yTest[j-240,i, 0] = highMag[i + j*BATCH_SIZE]
 
 
 session = tf.Session()
 
 session.run(tf.global_variables_initializer())
 
-for epoch in range(3):
+for epoch in range(2):
     epoch_error = 0
     i = 0
     j = 0
@@ -160,14 +163,13 @@ for epoch in range(3):
             inputs: (x),
             outputs: y,
         })[0]
-        print(".", end='')
 
     epoch_error /= ITERATIONS_PER_EPOCH
     valid_accuracy = session.run(accuracy, {
         inputs:  xTest,
         outputs: yTest,
     })
-    print("Epoch %d, train error: %.2f, valid accuracy: %.1f %%" % (epoch, epoch_error, valid_accuracy * 100.0))
+    print("Epoch %d, train error: %.4f" % (epoch, epoch_error))
 
 output = session.run(res, {
         inputs:  xTest,
@@ -181,8 +183,12 @@ print(error)
 
 
 plt.figure(2)
-plt.plot(t[240*BATCH_SIZE:300*BATCH_SIZE], output.flatten())
-plt.plot(t[240*BATCH_SIZE:300*BATCH_SIZE], highMag[240*BATCH_SIZE:300*BATCH_SIZE])
+plt.plot(t[n_*BATCH_SIZE:(n_+m)*BATCH_SIZE],output.flatten())
 plt.xlabel("P(E)")
 plt.ylabel("P(EQ > 5)")
+
+plt.figure(3)
+plt.plot(t[n*step:(n+m)*step], pwr[n*step:(n+m)*step])
+plt.xlabel("T")
+plt.ylabel("PWR")
 plt.show()
